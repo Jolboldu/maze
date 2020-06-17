9621deab2dd235c96a6a3b0149dcafabd07aa3f7
Jolboldu <sydykalyuulu_z@auca.kg>
2020-06-18 04:10:03 +0600

    added ai

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -1,21 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
     public float health = 100.0f;
+    private NavMeshAgent agent;
+    public PlayerController player;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+    
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      agent.SetDestination(player.transform.position);
     }
 
     public void TakeDamage(float amount)
